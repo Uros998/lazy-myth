793042a31044e212a16cc18f58ff41db5922402e
Uros998 <uros.milovanovic.4191@metropolitan.ac.rs>
2023-05-05 23:57:22 +0200

    added swiper

--- src/app/components/news-slider/news-slider.component.ts
+++ src/app/components/news-slider/news-slider.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {register} from 'swiper/element';
+// import Swiper styles
+import 'swiper/css';
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import {Autoplay, SwiperOptions} from "swiper";
 
 @Component({
   selector: 'app-news-slider',
   templateUrl: './news-slider.component.html',
   styleUrls: ['./news-slider.component.scss']
 })
-export class NewsSliderComponent implements OnInit {
+export class NewsSliderComponent implements OnInit, AfterViewInit {
+
+  swiperConfig: SwiperOptions = {
+    slidesPerView: 1,
+    direction: 'vertical',
+    autoplay: true,
+    grabCursor: true,
+    pagination: true,
+    mousewheel: true,
+    keyboard: true,
+    modules: [ Autoplay],
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    register();
+  }
 }
---
+++ src/app/directive/swiper.directive.ts
@@ -0,0 +1,21 @@
+import {Directive, ElementRef, Input} from '@angular/core';
+import {SwiperOptions} from "swiper";
+
+
+@Directive({
+  selector: '[appSwiper]'
+})
+export class SwiperDirective {
+  swiperElement: HTMLElement;
+
+  @Input('config')
+  config?: SwiperOptions;
+
+  constructor(private el: ElementRef<HTMLElement>) {
+    this.swiperElement = el.nativeElement;
+  }
+
+  ngAfterViewInit() {
+    Object.assign(this.el.nativeElement, this.config);
+  }
+}
